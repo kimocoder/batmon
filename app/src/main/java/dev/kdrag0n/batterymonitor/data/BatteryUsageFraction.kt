db8dab7f860527261406059f938544bb08963293
Danny Lin <danny@kdrag0n.dev>
2020-08-12 22:28:32 -0700

    Fix NaN usage when no time has been accumulated

--- app/src/main/java/dev/kdrag0n/batterymonitor/data/BatteryUsageFraction.kt
+++ app/src/main/java/dev/kdrag0n/batterymonitor/data/BatteryUsageFraction.kt
@@ -4,6 +4,10 @@ private const val NS_PER_SEC = 1e9
 
 data class BatteryUsageFraction(var usage: Double = 0.0, var timeNs: Long = 0) {
     fun perHour(): Double {
+        if (timeNs == 0L) {
+            return 0.0
+        }
+
         return usage / (timeNs / NS_PER_SEC / 60 / 60)
     }
 }
