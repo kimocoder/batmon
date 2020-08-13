e20388424670e4bf2b3648eeacedcbc9e664fe0d
Danny Lin <danny@kdrag0n.dev>
2020-08-12 19:18:23 -0700

    Keep monitor service alive with foreground notification

--- app/src/main/java/dev/kdrag0n/batterymonitor/service/MonitorService.kt
+++ app/src/main/java/dev/kdrag0n/batterymonitor/service/MonitorService.kt
@@ -1,35 +1,66 @@
 package dev.kdrag0n.batterymonitor.service
 
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import dev.kdrag0n.batterymonitor.R
+import dev.kdrag0n.batterymonitor.ui.MainActivity
+import timber.log.Timber
+
+private const val STATUS_NOTIFICATION_ID = 1
+private const val STATUS_CHANNEL_ID = "monitor_service_status_channel"
 
 class MonitorService : Service() {
     private var started = false
 
-    // Disallow binding since this service should run independently
-    override fun onBind(intent: Intent?): IBinder? = null
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.monitor_notification_channel_name)
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(STATUS_CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.monitor_notification_channel_description)
+
+            getSystemService<NotificationManager>()?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showForegroundNotification() {
+        // Launch MainActivity when the notification is tapped
+        val pendingIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+
+        val notification = NotificationCompat.Builder(this, STATUS_CHANNEL_ID)
+            .setContentTitle(getText(R.string.monitor_notification_title))
+            .setSmallIcon(R.drawable.ic_dashboard_black_24dp)
+            .setContentIntent(pendingIntent)
+            .setTicker(getText(R.string.monitor_notification_title))
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+
+        startForeground(STATUS_NOTIFICATION_ID, notification)
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // Avoid initializing the service multiple times
         if (!started) {
             started = true
 
-            // TODO: implement service
+            Timber.d("Starting...")
+            createNotificationChannel()
+            showForegroundNotification()
         }
 
         return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onCreate() {
-        super.onCreate()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
+    // Disallow binding since this service should run independently
+    override fun onBind(intent: Intent?): IBinder? = null
 
     companion object {
         fun start(context: Context) {
